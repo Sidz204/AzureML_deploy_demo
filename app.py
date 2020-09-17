89ade1a119fbacfa36d8217c1bdcac3395603e35
Siddhesh <shiva1210064@gmail.com>
2020-09-17 16:59:59 +0530

    first commit

--- app.py
+++ app.py
@@ -1,38 +1,41 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for
+from werkzeug import secure_filename
 import pickle
+import os
 
 
 app = Flask(__name__)
 model = pickle.load(open('randomForestRegressor.pkl','rb'))
-
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
 @app.route('/')
 def home():
-    #return 'Hello World'
     return render_template('home.html')
-    #return render_template('index.html')
+   
 
 @app.route('/predict',methods = ['POST'])
 def predict():
-    int_features = [float(x) for x in request.form.values()]
+
+    fl = request.files['file']
+    filename = secure_filename(fl.filename)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    fl.save(file_path)
+    with open(file_path) as f:
+        file_content = f.read()
+    
+    print(type(file_content))
+    print(file_content)
+
+    txt_list = file_content.split(',')
+
+    int_features = [float(x) for x in txt_list]
+    print("type---:",type(int_features))
+    print("values:",int_features)
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
-    print(prediction[0])
-
-    #output = round(prediction[0], 2)
-    return render_template('home.html', prediction_text="AQI for Jaipur {}".format(prediction[0]))
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trought request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
+    
+    return render_template('home.html', prediction_text="prediction output :{}".format(prediction[0]))
 
 
 
